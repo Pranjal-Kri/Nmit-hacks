dfaf78ac3f39ce561e4ef8109c6cc2eb1055c0b6
Pranjal Krishna Gupta <guptapranjalkrishna07@gmail.com>
2023-05-06 13:05:02 +0530

    Demo json file for sponsors form

---
+++ detailed_Sponsor.js
@@ -0,0 +1,27 @@
+const data = [
+    {
+        id: 1,
+        Comapny_name: "John Smith",
+        email: "tugrp@example.com",
+        sponsorship_type : "money ₹1000",
+        image: "https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB",
+        company_type : "Agriculture",
+        company_size : "Small",
+        company_location : "Dhaka",
+        company_description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed lorem sed lorem sed lorem sed lorem",
+        expectation :"20 nos Hording of size 12*25 "
+        },
+
+        {
+            id: 2,
+            Comapny_name: "ABC Corporation",
+            email: "abc@example.com",
+            sponsorship_type: "complimentaty food ",
+            image: "https://images.unsplash.com/photo-1581498210889-9f1e7dfee645?ixid=MnwxMjA3fDB",
+            company_type: "Food",
+            company_size: "Medium",
+            company_location: "Mumbai",
+            company_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vehicula, risus a pharetra aliquam, est lorem fringilla velit, sed ullamcorper risus sapien quis sapien. Cras venenatis mauris sit amet tellus gravida, at semper nisl malesuada.",
+            expectation: "promotion of there food products "
+            }
+]
