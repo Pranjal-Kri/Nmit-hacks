3b3923191435f661863dcfe9f8e5daabc57969ee
Pranjal Krishna Gupta <guptapranjalkrishna07@gmail.com>
2023-05-06 13:24:05 +0530

    Demo file for eventorganiser form

---
+++ detailed_Event.js
@@ -0,0 +1,43 @@
+const data =[
+    {
+        id : 1,
+        event_name : "John",
+        event_date : "2020-01-01",
+        event_type : "Tech fest",
+        event_topic : "Hackathon",
+        attendees_min : "100",
+        attendees_upto : "500",
+        location : "Bangalore",
+        event_description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices pos",
+        event_history : "It was held in 2021,2022 with average attendees of 250 person and our previous sponsers are Red bull, devfolio , filecoin ",
+        event_image : "https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQ",
+        event_video: "https://youtu.be/HoP_6lyGcKw",
+        budget_from : "₹10,000",
+        budget_to : "50,000",
+        event_status : "Upcoming",
+        gender_distribution_male : "60%",
+        gender_distribution_female : "40%",
+        event_organiser_email : "Ezam@gmail.com",
+    },
+    {
+        id: 2,
+        event_name: "TechX",
+        event_date: "2023-09-15",
+        event_type: "Conference",
+        event_topic: "AI and Machine Learning",
+        attendees_min: "200",
+        attendees_upto: "800",
+        location: "Delhi",
+        event_description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae",
+        event_history: "This is the first edition of the event",
+        event_image: "https://images.unsplash.com/photo-1595503102516-8d74c7e0a321?ixid=MnwxMjA3fDB",
+        event_video: "https://www.youtube.com/watch?v=7OuHJhjV7d4",
+        budget_from: "₹50,000",
+        budget_to: "1,50,000",
+        event_status: "Planning",
+        gender_distribution_male: "55%",
+        gender_distribution_female: "45%",
+        event_organiser_email: "techx@example.com"
+        }
+
+        ]
